0bb0d2edf7eb964f221b3142f524c18962bd02d8
Rahulkrsingh32 <rahulkrsingh32@gmail.com>
2022-01-21 20:57:59 +0530

    updated

--- server.js
+++ server.js
@@ -1,16 +1,3 @@
-/* const express = require('express')
-const app = express()
-const port = process.env.PORT || 5000
-const dbConnection = require('./db')
-app.use(express.json())
-
-app.use('/api/cars/' , require('./routes/carsRoute'))
-app.use('/api/users/' , require('./routes/usersRoute'))
-app.use('/api/bookings/' , require('./routes/bookingsRoute'))
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`node js server started in port ${port}`)) */
-
-
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
@@ -20,27 +7,7 @@ app.use(express.json())
 app.use('/api/cars/' , require('./routes/carsRoute'))
 app.use('/api/users/' , require('./routes/usersRoute'))
 app.use('/api/bookings/' , require('./routes/bookingsRoute'))
-
-
-const path = require('path')
-
-if(process.env.NODE_ENV==='production')
-{
-
-    app.use('/' , express.static('client/build'))
-
-    app.get('*' , (req , res)=>{
-
-          res.sendFile(path.resolve(__dirname, 'client/build/index.html'));
-
-    })
-
-}
-
 app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`node js server started in port ${port}`))
 
 
- 
-
-
-app.listen(port, () => console.log(`Node JS Server Started in Port ${port}`))
