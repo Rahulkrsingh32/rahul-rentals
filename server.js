330ce6ef43d21a444bef40db1876f00af906790f
Rahulkrsingh32 <rahulkrsingh32@gmail.com>
2022-01-22 16:22:51 +0530

    updated

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
 const port = process.env.PORT || 5000
 const dbConnection = require('./db')
-app.use(express.json())
 
+app.use(express.json())
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  }
+  );
 app.use('/api/cars/' , require('./routes/carsRoute'))
 app.use('/api/users/' , require('./routes/usersRoute'))
 app.use('/api/bookings/' , require('./routes/bookingsRoute'))
